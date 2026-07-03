b4e3b00354b3f8f33a64d550c8cbe37b61ab0aba
Hassan <hassanhome@Hassans-Mac-mini.local>
2026-07-03 08:16:06 +0800

    chore: refresh fifa data

--- src/data/fifa-fixtures.ts
+++ src/data/fifa-fixtures.ts
@@ -1174,7 +1174,7 @@ const OFFICIAL_FIFA_FIXTURES_JSON = String.raw`[
     "city": "Toronto",
     "status": "live",
     "homeScore": 0,
-    "awayScore": 0
+    "awayScore": 1
   },
   {
     "sourceUrl": "https://www.fifa.com/en/match-centre/match/17/285023/289287/400021519",
