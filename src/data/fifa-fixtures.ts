84e2fc6f9de829d9d1749b4630ce39b8f2fb1626
Hassan <hassanhome@Hassans-Mac-mini.local>
2026-07-05 03:01:05 +0800

    chore: refresh fifa data

--- src/data/fifa-fixtures.ts
+++ src/data/fifa-fixtures.ts
@@ -1272,7 +1272,7 @@ const OFFICIAL_FIFA_FIXTURES_JSON = String.raw`[
     "city": "Houston",
     "status": "live",
     "homeScore": 0,
-    "awayScore": 1
+    "awayScore": 2
   },
   {
     "sourceUrl": "https://www.fifa.com/en/match-centre/match/17/285023/289288/400021532",
